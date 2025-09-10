16d02437931e9970cd2c39b00e0748a4241e7e29
Audrey <audrey@grantski.com>
2025-09-09 21:49:50 -0600

    Completed get_column function

--- my_utils.py
+++ my_utils.py
@@ -1,2 +1,10 @@
 def get_column(file_name, query_column, query_value, result_column):
-    return None
+    result_array = []
+    read_file = open(file_name, 'r')
+    for line in read_file:
+        columns = line.strip('\n').split(',')
+        if columns[query_column] == query_value:
+            result_array = columns[result_column]
+            result_array.append(result_array[result_column])
+    read_file.close()
+    return result_array
